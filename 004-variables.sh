55d63392071e26466c2e92691cf4a2bfa9b57ee6
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-12 18:54:39 +0530

    auto save

--- 004-variables.sh
+++ 004-variables.sh
@@ -5,3 +5,6 @@ b=ten
 
 echo $a
 echo -e "\e[44;32m The value of a = $a \e[0m"
+echo -e "\e[43;33m The value of a = $a \e[0m"
+echo -e "\e[42;34m The value of a = $a \e[0m"
+echo -e "\e[41;35m The value of a = $a \e[0m"
