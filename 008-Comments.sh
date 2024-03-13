3ae9db9792d7b3b370cbacfc93a30246c978b253
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 16:30:29 +0530

    auto save

---
+++ 008-Comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#this is the single line comment
+
+#this is multiple line comment
+echo "this is not an comment"
+
+<<comment
+  echo "comment no 1"
+  echo "comment no 2"
+  echo "comment no 3"
+
+comment
+
+echo "this line is print out of the comment section"
