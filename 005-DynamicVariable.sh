1a3fd8a514ad7322e7dbb256785869a10460e9b3
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 14:06:13 +0530

    auto save

--- 005-DynamicVariable.sh
+++ 005-DynamicVariable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATE="13/MARCH/2024"
-No_Of_Sessions="4"
+DATE="$(date +%F)"
+No_Of_Sessions="wc -l"
 
 echo -e "This is todays Date ${DATE}"
 echo -e "No of Session Active ${No_Of_Sessions}"
