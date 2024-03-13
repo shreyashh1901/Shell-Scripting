823789d2c4769bdcfde628f420a2007df4d6a73c
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 14:40:33 +0530

    auto save

--- 006-SpecialVariables.sh
+++ 006-SpecialVariables.sh
@@ -7,5 +7,5 @@ CONTINENT=$3
 echo "this is special varialble : $a"
 echo "My script name is : $0"
 echo -e "My fav team is : \e[45;37m $TEAM \e[0m"
-echo -e "My fav team is : \e[46;36m $CITY \e[0m"
+echo -e "My fav team is : \e[90;36m $CITY \e[0m"
 echo -e "My fav team is : \e[47;35m $CONTINENT \e[0m"
