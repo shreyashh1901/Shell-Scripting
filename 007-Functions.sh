9baaca6c59208c34ae2bf5f825ef77aa14b71309
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 15:52:23 +0530

    auto save

--- 007-Functions.sh
+++ 007-Functions.sh
@@ -29,7 +29,7 @@ MYFUNC(){
     echo "word count : $(who|wc -l)"
     echo "print date : $(date +%F)"
     echo "print script name : $0"
-    echo "show cpu utilisation $(uptime)"
+    echo "show cpu utilisation $(uptime | awk -F : '{print $NF}' | awk -F ',' '{print $1}')"
     echo "Show PC Hostname $(hostname)"
 }
 MYFUNC
