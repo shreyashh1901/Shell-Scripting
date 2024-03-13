7aabfd5f9d94f294bcec5da370236505ff79279c
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 15:30:44 +0530

    auto save

---
+++ 007-Functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#there are 4 types of commands available
+
+# 1)Binary
+# 2)Aliases
+# 3)Shell Built-In commands
+# 4)Functions
+
+
+f()
+{
+    echo Hello World
+}
+
+f
+echo "we are learning devops"
+echo "my name is shreyash"
+
+f
