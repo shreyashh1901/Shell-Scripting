d53885e8d8eefb7b5d52405e2a37235505c6f846
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 13:51:58 +0530

    auto save

--- 005-DynamicVariable.sh
+++ 005-DynamicVariable.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DATE="13/03/2024"
-NOOFSESSIONS="4"
-echo -e "this is the current time $(DATE)"
-echo -e "this is the current time $(NOOFSESSIONS)"
+DATE="13/MARCH/2024"
+
+echo -e "This is todays Date ${DATE}"
