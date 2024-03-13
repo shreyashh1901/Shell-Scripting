9cd1397f3001a4f19c55aa336f35e00439ec0e43
Shreyash Dabir <shreyashdabir1990@gmail.com>
2024-03-13 14:23:57 +0530

    auto save

--- 006-SpecialVariables.sh
+++ 006-SpecialVariables.sh
@@ -2,6 +2,10 @@
 
 a=1990
 TEAM=$1
+TEAM=$2
+TEAM=$3
 echo "this is special varialble : $a"
 echo "My script name is : $0"
+echo "My fav team is : $TEAM"
+echo "My fav team is : $TEAM"
 echo "My fav team is : $TEAM"
